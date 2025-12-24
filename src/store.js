9c1e6e2c1fb8e8fb74a5a5b8aee6c60c949e8d37
MBANS8A1 <seanmbng@aol.com>
2025-12-24 18:05:31 +0000

    I mostly completed the reducer function in store.js. I added cases for currency amount deposit, withdrawal, loan request and paying back the loan. I will need to return to the "account/requestLoan" as the store is built out because it will need to make use of the loanPurpose state.

--- src/store.js
+++ src/store.js
@@ -4,4 +4,25 @@ const initialState = {
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      //return to
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
