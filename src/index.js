6347e4538b49266711a5dc952afb2363417a9716
MBANS8A1 <seanmbng@aol.com>
2025-12-26 14:51:44 +0000

    I have passed in the Redux store into the Provider application, in order to allow the connection between Redux and React.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
