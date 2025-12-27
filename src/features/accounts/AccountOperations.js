e3ff5b610347b1f19a54b3793c78f4d7008af2a3
MBANS8A1 <seanmbng@aol.com>
2025-12-27 15:36:51 +0000

    I added the code logic for the handleWithdrawal and handleRequestLoan event handler functions. I renamed the state argument in the useSelector to store (which is what it should be by convention) and destructured the loan from the store.account and renamed it currentLoan.

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { deposit } from "./accountSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, requestLoan, withdraw } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -10,6 +10,7 @@ function AccountOperations() {
   const [currency, setCurrency] = useState("USD");
 
   const dispatch = useDispatch();
+  const { loan: currentLoan } = useSelector((store) => store.account);
 
   function handleDeposit() {
     if (!depositAmount) return;
@@ -17,9 +18,18 @@ function AccountOperations() {
     setDepositAmount("");
   }
 
-  function handleWithdrawal() {}
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
 
-  function handleRequestLoan() {}
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+    dispatch(requestLoan(loanAmount, loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
 
   function handlePayLoan() {}
 
