910303fca19cc1f5365fdcb95c5e4e8d02ac0813
MBANS8A1 <seanmbng@aol.com>
2025-12-28 18:42:56 +0000

    I added some conditional rendering (via a ternary operator) to the button that fires the handleDeposit event. When the button is clicked the button will be disabled and the text will read "Converting...".

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -61,7 +61,9 @@ function AccountOperations() {
             <option value="EUR">Euro</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
