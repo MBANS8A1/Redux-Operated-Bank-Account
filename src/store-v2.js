09ecb84abb209cb56e79b8c37d67d33d2dd2bc5c
MBANS8A1 <seanmbng@aol.com>
2025-12-30 10:31:47 +0000

    I adjusted store-v2.js file and in thenew store.js file, all the other imports for thunk, applyMiddleware, combineReducers, createStore and composeWithDevTools are no longer needed as configureStore from RTK contains the aforementioned functionalities.

--- src/store-v2.js
+++ src/store-v2.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import { configureStore } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
--- src/store.js
+++ src/store.js
@@ -1,7 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
 import { configureStore } from "@reduxjs/toolkit";
-import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 const rootReducer = combineReducers({
