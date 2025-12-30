9d39f12a93c0e5b260d3a77389c1bbdab174f68c
MBANS8A1 <seanmbng@aol.com>
2025-12-30 10:36:33 +0000

    Also, I removed code for combining the reducers and creating the store from the rootReducer and thunk middleware because configureStore provides the needed functionality.

--- src/store.js
+++ src/store.js
@@ -1,14 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
