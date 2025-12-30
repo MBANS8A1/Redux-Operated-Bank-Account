512f48562c05f12db84466af70468a73b80be165
MBANS8A1 <seanmbng@aol.com>
2025-12-30 19:23:50 +0000

    I created the accountSlice using the createSlice() function from the Redux Toolkit package. It has a name property of "account", is supplied with the initialState object (used from classic Redux) and with a reducers object with actions corresponding to the names of the action a creators.  The logic for each reducer function matches the cases in the classic Redux reducer.

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -8,6 +8,31 @@ const initialState = {
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan(state, action) {
+      if (state.loan > 0) return;
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance += action.payload.amount;
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+console.log(accountSlice);
 //Reducer for accounts
 // export default function accountReducer(state = initialState, action) {
 //   switch (action.type) {
